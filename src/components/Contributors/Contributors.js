c700c8053e9abdb86fe6a51685375bdcf9e60860
Grace Manning <gracebmanning@gmail.com>
2024-08-30 16:00:43 -0700

    start contributors page

--- src/components/Contributors/Contributors.js
+++ src/components/Contributors/Contributors.js
@@ -4,8 +4,15 @@ export default function Contributors(){
     return(
         <div className='contributorsContainer'>
             <h3 className='contributorsTitle'>contributors</h3>
-            <div className='contributorsText'>
-                <p>
+            <div className='contributorTile'>
+                <h4 className='contributorName'>grace</h4>
+                <p className='contributorBio'>
+                    info here!
+                </p>
+            </div>
+            <div className='contributorTile'>
+                <h4 className='contributorName'>emmy</h4>
+                <p className='contributorBio'>
                     info here!
                 </p>
             </div>
