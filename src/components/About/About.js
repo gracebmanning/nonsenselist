42cf2ece50523663cc24687f91f438427a6688bc
Grace Manning <gracebmanning@gmail.com>
2024-08-30 22:01:15 -0700

    changed name to classifieds.lol

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import './About.css';
 export default function About(){
     return(
         <div className='aboutContainer'>
-            <h3 className='aboutTitle'>about nonsense list</h3>
+            <h3 className='aboutTitle'>about classifieds.lol</h3>
             <div className='aboutText'>
                 <p>
                     Created by <a href='https://grace-manning.com/' target='_blank' rel='noreferrer'>Grace Manning</a>.
